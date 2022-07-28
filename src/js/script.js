b1fdb5bd165a7114001c18828ea1f5364d0093ba
Vladislav <vladislav5008zo@gmail.com>
2022-07-28 16:49:30 +0300

    Правки стилей

--- src/js/script.js
+++ src/js/script.js
@@ -25,3 +25,22 @@ const barPercent = document.querySelectorAll('.skills__bar-percent'),
 barPercent.forEach((element, i) => {
     lineActive[i].style.width = barPercent[i].innerHTML;
 });
+
+//Smooth scroll and pageup
+
+	// flowing scroll
+	const anchors = document.querySelectorAll('a[href*="#"]')
+
+	for (let anchor of anchors) {
+	  anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+		
+		const blockID = anchor.getAttribute('href').substr(1)
+		
+		document.getElementById(blockID).scrollIntoView({
+		  behavior: 'smooth',
+		  block: 'start'
+		})
+	  })
+	}
+	//Smooth scroll and pageup end
